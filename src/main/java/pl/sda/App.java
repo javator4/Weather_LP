c00c91ea49792538c34fb0834374ab8800dc643d
LukaszPietrzak <pietrzakl90@gmail.com>
2019-07-13 15:56:17 +0200

    add implementation

--- src/main/java/pl/sda/App.java
+++ src/main/java/pl/sda/App.java
@@ -1,6 +1,7 @@
 package pl.sda;
 
 import org.apache.commons.io.IOUtils;
+import pl.sda.weatherService.WeatherService;
 
 /**
  * Hello world!
@@ -9,6 +10,12 @@ public class App {
 
     public static void main(String[] args) {
 
+        WeatherService weatherService = new WeatherService(
+                "http://api.apixu.com/v1/current.json",
+                "8480236ee7a3432fa6781558191307"
+                );
+
+        weatherService.getCityWeather("Torun");
         System.out.println("Hello World!");
     }
 }
--- src/main/java/pl/sda/weatherService/WeatherService.java
+++ src/main/java/pl/sda/weatherService/WeatherService.java
@@ -1,4 +1,55 @@
 package pl.sda.weatherService;
 
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+import pl.sda.model.Current;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
 public class WeatherService {
+
+    private String url;
+    private String key;
+    private String finalURL;
+
+
+    public WeatherService(String url, String key) {
+        this.url = url;
+        this.key = key;
+        this.finalURL = this.url + "?key=" + key + "&q=";
+
+    }
+
+    public Current getCityWeather(String city) {
+        this.finalURL = this.finalURL + city;
+        System.out.println(this.finalURL);
+
+        try {
+            String data = IOUtils.toString(new URL(this.finalURL),
+                    Charset.forName("UTF-8"));
+            JSONObject jsonobject = new JSONObject(data);
+            String temp = jsonobject.getJSONObject("current").get("temp_c").toString();
+            System.out.println(temp);
+
+            Current current = new Current();
+            current.setHumidity(Integer.parseInt(temp));
+            current.setWind_kmph(Float.parseFloat(temp));
+            current.setIs_day(Integer.parseInt(temp));
+            current.setCloud(Integer.parseInt(temp));
+            current.setWind_degree(Integer.parseInt(temp));
+            current.setTemp_c(Float.parseFloat(temp));
+
+            return current;
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+
 }
