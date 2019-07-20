f1fbfae32b8820c988468d7fb3f2d222f443c922
LukaszPietrzak <pietrzakl90@gmail.com>
2019-07-20 10:31:33 +0200

    add abstraction class Json Data

---
+++ src/main/java/pl/sda/AbstractJsonData.java
@@ -0,0 +1,33 @@
+package pl.sda;
+
+import org.apache.commons.io.IOUtils;
+import pl.sda.model.Weather;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+public abstract class AbstractJsonData {
+
+    private String url;
+    private String apiKey;
+    private String finalURL;
+    private String data = "";
+
+    public String getJSONData(String city) {
+        if (this.data.isEmpty()) {
+            this.finalURL = this.finalURL + city;
+            System.out.println("WYWOLANIE");
+            try {
+                this.data = IOUtils.toString(new URL(this.finalURL),
+                        Charset.forName("UTF-8"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return data;
+    }
+
+    abstract Weather getWeather();
+
+}
